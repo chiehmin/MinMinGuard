b87fa05d482e9d79c6507030645e1f626c5a0b1a
FatMinMin <chiehmin18@gmail.com>
2015-02-15 18:28:55 +0800

    using android studio now

---
+++ actionbarsherlock/build/generated/source/buildConfig/release/com/actionbarsherlock/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.actionbarsherlock;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.actionbarsherlock";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 440;
+  public static final String VERSION_NAME = "";
+}
---
+++ build/generated/source/buildConfig/debug/tw/fatminmin/xposed/minminguard/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package tw.fatminmin.xposed.minminguard;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "tw.fatminmin.xposed.minminguard";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 30;
+  public static final String VERSION_NAME = "";
+}
