cc8b2b58cdccf988a6d01cbe2d68b12425eba37b
FatMinMin <chiehmin18@gmail.com>
2015-10-01 17:47:18 +0800

    start refactor MinMinGuard

--- src/tw/fatminmin/xposed/minminguard/adnetwork/Yahoo.java
+++
@@ -1,8 +0,0 @@
-package tw.fatminmin.xposed.minminguard.adnetwork;
-
-public class Yahoo {
-
-    public final static String bannerPrefix = "com.yahoo.mobile.client.share.android.ads.views";
-    public final static String banner = "com.yahoo.mobile.client.android.weather.ui.view.AdView";
-
-}
