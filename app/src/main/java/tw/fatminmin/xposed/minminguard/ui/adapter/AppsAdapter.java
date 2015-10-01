24006966677a787c4d87864dd37ce79d02758763
FatMinMin <chiehmin18@gmail.com>
2015-10-02 02:34:53 +0800

    add mode change button

--- app/src/main/java/tw/fatminmin/xposed/minminguard/ui/adapter/AppsAdapter.java
+++ app/src/main/java/tw/fatminmin/xposed/minminguard/ui/adapter/AppsAdapter.java
@@ -10,7 +10,6 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.CompoundButton;
 import android.widget.ImageView;
 import android.widget.Switch;
 import android.widget.TextView;
