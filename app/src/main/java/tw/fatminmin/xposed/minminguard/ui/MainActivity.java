e775b27512744e36081033f23f6cb28a98ae38ca
FatMinMin <chiehmin18@gmail.com>
2015-10-03 20:39:47 +0800

    add navigation item

--- app/src/main/java/tw/fatminmin/xposed/minminguard/ui/MainActivity.java
+++ app/src/main/java/tw/fatminmin/xposed/minminguard/ui/MainActivity.java
@@ -1,6 +1,9 @@
 package tw.fatminmin.xposed.minminguard.ui;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.preference.PreferenceManager;
+import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
@@ -17,6 +20,26 @@ public class MainActivity extends AppCompatActivity {
 
     private DrawerLayout mDrawerLayout;
     private ActionBarDrawerToggle mToggle;
+    private NavigationView mNavigationView;
+
+    private NavigationView.OnNavigationItemSelectedListener mNavListener = new NavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(MenuItem menuItem) {
+            mDrawerLayout.closeDrawers();
+            switch (menuItem.getItemId()) {
+                case R.id.action_settings:
+                    break;
+                case R.id.action_donate:
+                    Intent it = new Intent(Intent.ACTION_VIEW,
+                            Uri.parse("https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=47GXTVHUB4LYS"));
+                    startActivity(it);
+                    break;
+                case R.id.action_about:
+                    break;
+            }
+            return true;
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +50,10 @@ public class MainActivity extends AppCompatActivity {
         getSupportActionBar().setHomeButtonEnabled(true);
 
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mNavigationView = (NavigationView) findViewById(R.id.navigation_view);
+
+        mNavigationView.setNavigationItemSelectedListener(mNavListener);
+
         mToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.app_name , R.string.app_name) {
             @Override
             public void onDrawerClosed(View drawerView) {
