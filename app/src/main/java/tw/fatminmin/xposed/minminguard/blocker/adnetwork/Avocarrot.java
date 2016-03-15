84a32894a9497a3da9577baf1d2f8d35c5f94498
Elton Dakaj <elton235@gmail.com>
2016-03-15 00:52:11 -0400

    Added the Avocarrot ad network, in order to block the ad that pops up in Casper after you send a Snap. (Using helper function)

--- app/src/main/java/tw/fatminmin/xposed/minminguard/blocker/adnetwork/Avocarrot.java
+++ app/src/main/java/tw/fatminmin/xposed/minminguard/blocker/adnetwork/Avocarrot.java
@@ -1,58 +1,26 @@
 package tw.fatminmin.xposed.minminguard.blocker.adnetwork;
 
-import android.view.View;
-
-import de.robv.android.xposed.XC_MethodHook;
-import de.robv.android.xposed.XposedBridge;
-import de.robv.android.xposed.XposedHelpers;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
-import tw.fatminmin.xposed.minminguard.Main;
+import tw.fatminmin.xposed.minminguard.blocker.ApiBlocking;
 import tw.fatminmin.xposed.minminguard.blocker.Blocker;
-import tw.fatminmin.xposed.minminguard.blocker.Util;
-
-import static de.robv.android.xposed.XposedHelpers.findClass;
 
 public class Avocarrot extends Blocker {
 
-    public static final String inter = "com.avocarrot.androidsdk.AvocarrotInterstitial";
-    public static final String interPrefix = "com.avocarrot.androidsdk";
+    public static final String prefix = "com.avocarrot.androidsdk";
+
+    public static final String adController = "com.avocarrot.androidsdk.BaseController";
+    public static final String adControllerFunc = "loadAd";
 
     @Override
     public boolean handleLoadPackage(final String packageName, XC_LoadPackage.LoadPackageParam lpparam, final boolean removeAd) {
-        try {
-
-            Class<?> avocarrotInter = findClass(inter, lpparam.classLoader);
-
-            XposedBridge.hookAllMethods(avocarrotInter, "loadAndShowAd", new XC_MethodHook() {
-
-                @Override
-                protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-
-                    Util.log(packageName, "Detect avocarrotInter loadAndShowAd in " + packageName);
-
-                    if (removeAd) {
-                        param.setResult(new Object());
-                        Main.removeAdView((View) param.thisObject, packageName, true);
-                    }
-                }
-
-            });
-
-            Util.log(packageName, packageName + " uses avocarrot");
-        }
-        catch(XposedHelpers.ClassNotFoundError e) {
-            return false;
-        }
-        return true;
+        boolean result = false;
+        result |= ApiBlocking.blockAdFunction(packageName, adController, adControllerFunc, lpparam, removeAd);
+        return result;
     }
 
     @Override
-    public String getBanner() {
-        return null;
-    }
+    public String getBanner() { return null; }
 
     @Override
-    public String getBannerPrefix() {
-        return interPrefix;
-    }
-}
+    public String getBannerPrefix() { return prefix; }
+}   
