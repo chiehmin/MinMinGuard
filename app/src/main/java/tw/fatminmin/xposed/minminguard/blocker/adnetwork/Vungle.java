b2b3cf3a5136de251752297b62e7fe006484f824
MSkjel <fuckbio@hotmail.com>
2019-03-12 01:33:18 +0100

    Added Vungle.Publisher

--- app/src/main/java/tw/fatminmin/xposed/minminguard/blocker/adnetwork/Vungle.java
+++ app/src/main/java/tw/fatminmin/xposed/minminguard/blocker/adnetwork/Vungle.java
@@ -6,14 +6,16 @@ import tw.fatminmin.xposed.minminguard.blocker.Blocker;
 
 public class Vungle extends Blocker
 {
-    public static final String VUNGLE = "com.vungle.warren.Vungle";
+    public static final String VUNGLE_WARREN = "com.vungle.warren.Vungle";
+    public static final String VUNGLE_PUBLISHER = "com.vungle.publisher.VunglePub";
 
     @Override
     public boolean handleLoadPackage(String packageName, XC_LoadPackage.LoadPackageParam lpparam, boolean removeAd)
     {
         boolean result = false;
 
-        result |= ApiBlocking.blockAdFunction(packageName, VUNGLE, "canPlayAd", "com.vungle.warren.model.Advertisement", lpparam, removeAd);
+        result |= ApiBlocking.blockAdFunction(packageName, VUNGLE_WARREN, "canPlayAd", "com.vungle.warren.model.Advertisement", lpparam, removeAd);
+        result |= ApiBlocking.blockAdFunction(packageName, VUNGLE_PUBLISHER, "loadAd", String.class, lpparam, removeAd);
 
         return result;
     }
